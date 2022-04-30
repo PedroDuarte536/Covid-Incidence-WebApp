1988d78abdb840e554671c27977266493a7c0f08
Pedro Duarte <pedro.dld536@gmail.com>
2022-04-30 19:10:02 +0100

    Add DatePeriod Structure

--- projAPI/src/main/java/com/tqs/hw/api/data/DatePeriod.java
+++ projAPI/src/main/java/com/tqs/hw/api/data/DatePeriod.java
@@ -1,5 +1,17 @@
 package com.tqs.hw.api.data;
 
+import java.util.Date;
+
 public class DatePeriod extends Period {
+
+  private final Date date;
+
+  public DatePeriod(Date date) {
+    this.date = date;
+  }
+
+  public Date getDate() {
+    return this.date;
+  }
   
 }
