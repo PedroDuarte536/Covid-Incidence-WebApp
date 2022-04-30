e15f93114f65589c3fcb063ff67f92b2caedb950
Pedro Duarte <pedro.dld536@gmail.com>
2022-04-30 19:10:59 +0100

    Add methods for cache metrics

--- projAPI/src/main/java/com/tqs/hw/api/http/CachedHttpClient.java
+++ projAPI/src/main/java/com/tqs/hw/api/http/CachedHttpClient.java
@@ -3,8 +3,16 @@ package com.tqs.hw.api.http;
 public class CachedHttpClient implements HttpClient {
 
   @Override
-  public String get(String url) {
+  public String get (String url) {
     return null;
+  }
+
+  public String getFromCache (String url) {
+    return null;
+  }
+
+  public int getAmountOfCachedResponses() {
+    return 0;
   }
   
 }
