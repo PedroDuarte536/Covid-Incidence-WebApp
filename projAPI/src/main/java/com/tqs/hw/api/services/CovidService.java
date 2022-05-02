5eeeb6f33bf3af1802a366459d2eb6382b180dc5
Pedro Duarte <pedro.dld536@gmail.com>
2022-05-02 19:29:15 +0100

    Simplify alternation between data providers

--- projAPI/src/main/java/com/tqs/hw/api/services/CovidService.java
+++ projAPI/src/main/java/com/tqs/hw/api/services/CovidService.java
@@ -25,12 +25,6 @@ public class CovidService implements DataProvider {
     new VacCovidDataProvider(httpClient)
   };
 
-  private Integer currentProvider = null;
-
-  public CovidService () {
-    if (providers.length > 0) currentProvider = 0;
-  }
-
   public interface ProviderFunc {
     public Metric getMetric (DataProvider provider);
   }
@@ -38,11 +32,9 @@ public class CovidService implements DataProvider {
   public Metric call(ProviderFunc func) {
     Metric result = new Metric("", null, null);
 
-    if (currentProvider != null) {
-      for (DataProvider provider : providers) {
-        Metric res = func.getMetric(provider);
-        if (res != null) result = res;
-      }
+    for (DataProvider provider : providers) {
+      Metric res = func.getMetric(provider);
+      if (res != null && res.getValue() != null) result = res;
     }
 
     return result;
@@ -79,16 +71,6 @@ public class CovidService implements DataProvider {
 
   public void setProviders(DataProvider[] dataProviders) {
     this.providers = dataProviders;
-  }
-
-  public Integer getCurrentProvider() {
-    return this.currentProvider;
-  }
-
-  public void setCurrentProvider(Integer currentProvider) {
-    if (currentProvider != null && (currentProvider < 0 || currentProvider >= providers.length)) currentProvider = null;
-
-    this.currentProvider = currentProvider;
   }
   
 }
